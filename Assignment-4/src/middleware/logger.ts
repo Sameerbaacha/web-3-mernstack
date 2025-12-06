894255568e1997f6b7ad47002ccf3493f1d186ee
Sameerbaacha <153771689+Sameerbaacha@users.noreply.github.com>
2025-12-06 13:46:11 +0500

    Delete Assignment-4 directory

--- Assignment-4/src/middleware/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-const logger = (req: Request, res: Response, next: NextFunction) => {
-    const time = new Date().toLocaleString();
-    console.log(`[${time}] ${req.method} ${req.originalUrl}`);
-    next();
-};
-
-export default logger;
